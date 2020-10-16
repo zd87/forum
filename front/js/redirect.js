454b2ed37c43a2696617070ee0409c120b30a8ff
zd87 <50809254+zd87@users.noreply.github.com>
2020-10-16 23:25:51 +0200

    clean up

--- front/js/redirect.js
+++ front/js/redirect.js
@@ -1,6 +1,4 @@
 const toPage=(pageName, query)=>{
-    console.log(pageName, query);
-    
     let routes = {
         "home":"/pages/homePage/homePage.html",
         "discussion":"/pages/discussionPage/discussionPage.html"
