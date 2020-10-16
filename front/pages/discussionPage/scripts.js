962e852bbda4779828b82a083637d007f6acfeeb
zd87 <50809254+zd87@users.noreply.github.com>
2020-10-16 22:30:10 +0200

    refactoring: extracting unique dom elements

--- front/pages/discussionPage/scripts.js
+++ front/pages/discussionPage/scripts.js
@@ -1,38 +1,31 @@
 import * as ajax from "../../js/ajax.js";
-import * as utilsHtml from "../../js/utilsHtml.js";
+import { buildComment, el } from "../../js/utilsHtml.js";
 import { formDataToJson } from "../../js/utilsForm.js";
 import { storage } from "../../data/storage.js";
 
 const displayComments = (comments)=> {
-    const commentsContainer = document.getElementById("commentsContainer");
-    commentsContainer.innerHTML="";
+    el.commentsContainer.innerHTML="";
     for(let comment of comments) {
-        utilsHtml.buildComment(comment, commentsContainer, addQuote);
+        buildComment(comment, commentsContainer, addQuote);
     }
 }
 
 const displayTopic=(topic)=>{
-    let topicName = document.getElementById("chosenTopic");
-    let topicDescription = document.getElementById("chosenTopicDescription");
-    let topicCreationDate = document.getElementById("chosenTopicCreationDate");
-    topicName.innerHTML = topic.name;
-    topicDescription.innerHTML = topic.description;
-    topicCreationDate.innerHTML = topic.creationDate;
+    el.topicName.innerHTML = topic.name;
+    el.topicDescription.innerHTML = topic.description;
+    el.topicCreationDate.innerHTML = topic.creationDate;
 }
 
 const toggleQuote =(display, comment)=>{
-    let quotedComment = document.getElementById("quotedComment");
-    quotedComment.style.display=display;    
+    el.quotedComment.style.display=display;    
     if(comment){
-        let quotedCommentBody = document.getElementById("quotedCommentBody");
-        quotedCommentBody.innerHTML="";
-        quotedCommentBody.innerHTML=comment.comment;
+        el.quotedCommentBody.innerHTML="";
+        el.quotedCommentBody.innerHTML=comment.comment;
         initRemoveButton();
     }
 }
 const initRemoveButton=()=>{
-    const removeQuoteButton = document.getElementById("removeQuote");
-    removeQuoteButton.onclick = function(){ removeQuote() };
+    el.removeQuoteButton.onclick = function(){ removeQuote() };
 }
 const addQuote= (comment) => {
     storage.comments.quotedCommentId = comment.id;
@@ -44,8 +37,7 @@ const removeQuote =()=>{
 }
 const submitComment =(event)=>{
     event.preventDefault();
-    let form = document.getElementById("commentForm");
-    let data = new FormData(form);
+    let data = new FormData(el.commentForm);
     if(storage.comments.quotedCommentId) data.set("quotedCommentId", storage.comments.quotedCommentId);
     let json = formDataToJson(data);
     ajax.post(`http://localhost:8085/topics/${storage.topics.chosenTopic}/comments`, fetchComments, json); 
@@ -58,7 +50,7 @@ const discussionInit=()=>{
         if (key==="topicId" && value) storage.topics.chosenTopic=value;
         fetchTopic();
         fetchComments();
-        document.getElementById("submitComment").addEventListener("click", submitComment);
+        el.submitCommentButton.addEventListener("click", submitComment);
     }
 }
 
